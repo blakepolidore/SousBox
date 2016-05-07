3273ca786d96d08de7e7d1afa502b0aa9668ead0
Billy <Billy@BILLs-MacBook-Pro.local>
2016-05-06 17:13:52 -0700

    work on filter and facebook

--- SousBox/app/src/main/java/com/example/billy/sousbox/fragments/PreferencesFragment.java
+++ SousBox/app/src/main/java/com/example/billy/sousbox/fragments/PreferencesFragment.java
@@ -1,10 +1,13 @@
 package com.example.billy.sousbox.fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +15,23 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.billy.sousbox.R;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.sql.Time;
+import java.util.Map;
+
+import timber.log.Timber;
 
 /**
  * Created by Billy on 5/2/16.
@@ -25,12 +39,29 @@ import com.facebook.login.widget.LoginButton;
 public class PreferencesFragment extends Fragment {
 
     private TextView info;
+
+
+    /* The login button for Facebook */
+    /* The callback manager for Facebook */
+    /* Used to track user logging in/out off Facebook */
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    private AccessTokenTracker mFacebookAccessTokenTracker;
+
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
     private FrameLayout fragContainer;
     private FoodListsMainFragment recipeListsFrag;
+    private ProgressDialog mAuthProgressDialog;
+
+    /* A reference to the Firebase */
+    private Firebase mFirebaseRef;
+
+    /* Data from the authenticated user */
+    private AuthData mAuthData;
+
+    /* Listener for Firebase session changes */
+    private Firebase.AuthStateListener mAuthStateListener;
 
 
     @Nullable
@@ -40,12 +71,38 @@ public class PreferencesFragment extends Fragment {
         FacebookSdk.sdkInitialize(v.getContext());
         callbackManager = CallbackManager.Factory.create();
 
+
+        mAuthProgressDialog = new ProgressDialog(getContext());
+        mAuthProgressDialog.setTitle("Loading");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+        mAuthProgressDialog.show();
+        mAuthStateListener = new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                mAuthProgressDialog.hide();
+                setAuthenticatedUser(authData);
+            }
+        };
+        /* Check if the user is authenticated with Firebase already. If this is the case we can set the authenticated
+         * user and hide hide any login buttons */
+        mFirebaseRef.addAuthStateListener(mAuthStateListener);
+
+
         loginButton = (LoginButton)v.findViewById(R.id.login_button);
         loginButton.setFragment(this);
         info = (TextView)v.findViewById(R.id.info);
         fragContainer = (FrameLayout)v.findViewById(R.id.fragment_container_id);
         recipeListsFrag = new FoodListsMainFragment();
         fragmentManager = getFragmentManager();
+
+        mFacebookAccessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+               Timber.i("Facebook.AccessTokenTracker.OnCurrentAccessTokenChanged");
+                PreferencesFragment.this.onFacebookAccessTokenChange(currentAccessToken);
+            }
+        };
 
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -76,6 +133,124 @@ public class PreferencesFragment extends Fragment {
         return v;
     }
 
+    private void onFacebookAccessTokenChange(AccessToken token) {
+        if (token != null) {
+            mAuthProgressDialog.show();
+            mFirebaseRef.authWithOAuthToken("facebook", token.getToken(), new AuthResultHandler("facebook"));
+        } else {
+            // Logged out of Facebook and currently authenticated with Firebase using Facebook, so do a logout
+            if (this.mAuthData != null && this.mAuthData.getProvider().equals("facebook")) {
+                mFirebaseRef.unauth();
+                //setAuthenticatedUser(null);
+            }
+        }
+    }
 
+    /**
+     * Unauthenticate from Firebase and from providers where necessary.
+     */
+    private void logout() {
+        if (this.mAuthData != null) {
+            /* logout of Firebase */
+            mFirebaseRef.unauth();
+            /* Logout of any of the Frameworks. This step is optional, but ensures the user is not logged into
+             * Facebook/Google+ after logging out of Firebase. */
+            if (this.mAuthData.getProvider().equals("facebook")) {
+                /* Logout from Facebook */
+                LoginManager.getInstance().logOut();
+            }
+            /* Update authenticated user and show login buttons */
+            setAuthenticatedUser(null);
+        }
+    }
+
+    /**
+     * This method will attempt to authenticate a user to firebase given an oauth_token (and other
+     * necessary parameters depending on the provider)
+     */
+    private void authWithFirebase(final String provider, Map<String, String> options) {
+        if (options.containsKey("error")) {
+            showErrorDialog(options.get("error"));
+        } else {
+            mAuthProgressDialog.show();
+            if (provider.equals("twitter")) {
+                // if the provider is twitter, we pust pass in additional options, so use the options endpoint
+                mFirebaseRef.authWithOAuthToken(provider, options, new AuthResultHandler(provider));
+            } else {
+                // if the provider is not twitter, we just need to pass in the oauth_token
+                mFirebaseRef.authWithOAuthToken(provider, options.get("oauth_token"), new AuthResultHandler(provider));
+            }
+        }
+    }
+
+    /**
+     * Once a user is logged in, take the mAuthData provided from Firebase and "use" it.
+     */
+    private void setAuthenticatedUser(AuthData authData) {
+        if (authData != null) {
+            /* Hide all the login buttons */
+            loginButton.setVisibility(View.GONE);
+
+            /* show a provider specific status text */
+            String name = null;
+            if (authData.getProvider().equals("facebook")) {
+                name = (String) authData.getProviderData().get("displayName");
+            }
+            else {
+                Timber.i("Invalid provider: " + authData.getProvider());
+            }
+            if (name != null) {
+//                mLoggedInStatusTextView.setText("Logged in as " + name + " (" + authData.getProvider() + ")");
+            }
+        } else {
+            /* No authenticated user show all the login buttons */
+            loginButton.setVisibility(View.VISIBLE);
+        }
+        this.mAuthData = authData;
+        /* invalidate options menu to hide/show the logout button */
+
+    }
+
+    /**
+     * Show errors to users
+     */
+    private void showErrorDialog(String message) {
+        new AlertDialog.Builder(getContext())
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    /**
+     * Utility class for authentication results
+     */
+    private class AuthResultHandler implements Firebase.AuthResultHandler {
+
+        private final String provider;
+
+        public AuthResultHandler(String provider) {
+            this.provider = provider;
+        }
+
+        @Override
+        public void onAuthenticated(AuthData authData) {
+            mAuthProgressDialog.hide();
+            Timber.i(provider + " auth successful");
+            setAuthenticatedUser(authData);
+        }
+
+        @Override
+        public void onAuthenticationError(FirebaseError firebaseError) {
+            mAuthProgressDialog.hide();
+            showErrorDialog(firebaseError.toString());
+        }
+    }
+
+    private void loginAnonymously() {
+        mAuthProgressDialog.show();
+        mFirebaseRef.authAnonymously(new AuthResultHandler("anonymous"));
+    }
 
 }
